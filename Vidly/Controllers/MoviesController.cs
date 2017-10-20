9c68348fb6359863a51356d5481c10bb667d314a
vsantiagos <vsantiagos>
2017-10-20 18:54:22 -0500

    core: Clase 11

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id= " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 5;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex at {0} and sortBy {1}", pageIndex, sortBy));
+        }
+    }
+}
